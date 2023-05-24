65db9564c895153deb530ea493f94e8ebfc4afcf
jwcho99 <100013870+jwcho99@users.noreply.github.com>
2023-05-25 04:43:47 +0900

    Update 미로 탐색.cc

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,39 +1,49 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
 #include <stack>
+#include <tuple>
+#include <cmath>
+#include <cstring>
+#include <vector>
 using namespace std;
 
-int map[101][101]={};
-int visited[101][101]={};
 int n,m;
+vector<vector<int>> maze;
+vector<vector<int>> visited;
 int dx[4]={1,-1,0,0};
 int dy[4]={0,0,1,-1};
 
 int main(){
-    queue<pair<int,int>> q;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> m;
+    maze=vector<vector<int>>(n,vector<int>(m,0));
+    visited=vector<vector<int>>(n,vector<int>(m,0));
     for(int i=0;i<n;i++){
+        string s;
+        cin >> s;
         for(int j=0;j<m;j++){
-            scanf("%1d", &map[i][j]);
+            maze[i][j]=s[j]-'0';
         }
     }
+
+    queue<pair<int,int>> q;
     q.push({0,0});
     visited[0][0]=1;
     while(!q.empty()){
-        int x,y,nx,ny;
-        x=q.front().first;
-        y=q.front().second;
+        int x=q.front().first, y=q.front().second;
         q.pop();
         for(int i=0;i<4;i++){
-            nx=x+dx[i];
-            ny=y+dy[i];
-            if (nx<0 || nx>=n || ny<0 || ny>=m) continue;
-            if (map[nx][ny]==0 || visited[nx][ny]!=0) continue;
-
-            visited[nx][ny]=visited[x][y]+1;
-            q.push({nx,ny});
+            int nx=x+dx[i], ny=y+dy[i];
+            if(0<=nx && nx<n && 0<=ny && ny<m){
+                if(maze[nx][ny]==1 && !visited[nx][ny]){
+                    q.push({nx,ny});
+                    visited[nx][ny]=visited[x][y]+1;
+                }
+            }
         }
-
     }
     cout << visited[n-1][m-1] << "\n";
 }

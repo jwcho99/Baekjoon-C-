860ef4096799e2c15a3d58dd442097cce0a613af
jwcho99 <100013870+jwcho99@users.noreply.github.com>
2023-09-18 00:34:45 +0900

    [Silver IV] Title: 대칭 차집합, Time: 64 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -10,49 +10,60 @@
 #include <sstream>
 using namespace std;
 
+const int SIZE=2e5+4;
+const int MOD=1e5+7;
 
-int n,m;
-int hashn;
-vector<vector<int>> hash_table;
+struct Nodepool{
+    Nodepool* next;
+    int data;
+}nodepool[SIZE];
 
-void addfunc(int target){
-    int k=target%hashn;
-    hash_table[k].push_back(target);
+int nodecnt;
+Nodepool* getNode(int d){
+    nodepool[nodecnt].next=NULL;
+    nodepool[nodecnt].data=d;
+    return &nodepool[nodecnt++];
 }
+struct Hash{
+    Nodepool* head[MOD];
+    void push(int data){
+        int h=data%MOD;
+        Nodepool* node=getNode(data);
 
-bool search(int target){
-    int k=target%hashn;
-    if(hash_table[k].size()==0) return false;
-    else{
-        for(int x:hash_table[k]){
-            if(x==target) return true;
+        node->next=head[h];
+        head[h]=node;
+    }
+    bool find(int key){
+        int h=key%MOD;
+
+        Nodepool* cur=head[h];
+        while(cur){
+            if(cur->data==key) return true;
+            cur=cur->next;
         }
         return false;
     }
-}
+}hashMap;
+
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
+    int n,m;
+    int input;
     cin >> n >> m;
-    if(n>=m) hashn=n;
-    else hashn=m;
-
-    //vector<vector<int>> hash_table(hashn+1);
-    hash_table = vector<vector<int>>(hashn+1);
-    
-    int cmd;
     for(int i=0;i<n;i++){
-        cin >> cmd;
-        addfunc(cmd);
+        cin >> input;
+        hashMap.push(input);
     }
 
-    int total=0;
+    int cnt=0;
     for(int i=0;i<m;i++){
-        cin >> cmd;
-        if(search(cmd)) total+=1;
+        cin >> input;
+        cnt+=hashMap.find(input);
     }
-    cout << n+m-2*total << "\n";
+
+    cout << n+m-2*cnt << "\n";
 
 }

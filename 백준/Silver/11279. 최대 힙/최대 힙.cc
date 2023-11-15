a2ded0dd742d740868203ebe07fcc560784fb22e
jwcho99 <100013870+jwcho99@users.noreply.github.com>
2023-11-15 14:37:11 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.cc
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -1,28 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX_SIZE=1e5;
+
+struct MAX_Heap{
+	int heap_array[MAX_SIZE];
+	int size=0;
+
+	int parent(int index){
+		return (index-1)/2;
+	}
+	int leftChild(int index){
+		return 2*index+1;
+	}
+	int rightChild(int index){
+		return 2*index+2;
+	}
+	void heapifyUp(int index){
+		while(index>0 && heap_array[index]>heap_array[parent(index)]){
+			swap(heap_array[index], heap_array[parent(index)]);
+			index=parent(index);
+		}
+	}
+
+	void heapifyDown(int index){
+		int Maxindex=index;
+		int left=leftChild(index);
+		int right=rightChild(index);
+		if(left<size && heap_array[left]>heap_array[Maxindex]) Maxindex=left;
+		if(right<size && heap_array[right]>heap_array[Maxindex]) Maxindex=right;
+
+		if(index!=Maxindex){
+			swap(heap_array[index], heap_array[Maxindex]);
+			heapifyDown(Maxindex);
+		}
+	}
+
+	void insert(int data){
+		heap_array[size]=data;
+		size++;
+		heapifyUp(size-1);
+	}
+
+	int extractMax(){
+		if(size<=0){
+			return 0;
+		}
+		int Max=heap_array[0];
+		heap_array[0]=heap_array[size-1];
+		size--;
+		heapifyDown(0);
+		return Max;
+	}
+};
+
 int n;
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-	priority_queue<int, vector<int>, less<int>> pq;
 	cin >> n;
+	MAX_Heap max_heap;
 
 	for(int i=0;i<n;i++){
 		int cmd; cin >> cmd;
-		if(cmd == 0){
-			if(pq.empty()){
-				cout << 0 << "\n";
-				continue;
-			}
-			cout << pq.top() << "\n";
-			pq.pop();
+		if(cmd==0){
+			int max=max_heap.extractMax();
+			cout << max << "\n";
 		}
 		else{
-			pq.push(cmd);
+			max_heap.insert(cmd);
 		}
 	}
-
 }

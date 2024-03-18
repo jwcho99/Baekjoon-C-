f4a828f3d6cc3a6fe8be0ce6294ad64b0ea919f2
jwcho99 <100013870+jwcho99@users.noreply.github.com>
2024-03-18 22:50:47 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -25,7 +25,7 @@ int main(){
     for(int k=1;k<=n;k++){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+                if(dist[i][j]>dist[i][k]+dist[k][j]) dist[i][j]=dist[i][k]+dist[k][j];
             }
         }
     }
